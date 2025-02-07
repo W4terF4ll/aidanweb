617a007bfcbd3a05fab751da0aa8ed75976acba7
Aidan Jozefiak <56806946+W4terF4ll@users.noreply.github.com>
2025-02-06 21:13:59 -0500

    Add files via upload

--- v1/src/main.js
+++ v1/src/main.js
@@ -1,19 +1,15 @@
-titles = ["Welcome!", "Education", "Skills", "Projects!", "Favorite Media!", "Aidan's Top Website Picks!", "Class Projects"]
-
 function reveal(num) {
-    const center = document.getElementById("divCenterWrapper");
+    const center = document.getElementById("divCenter");
     center.classList.remove('changed');
     void center.offsetWidth;
     center.classList.add('changed');
 
     const divsToHide = document.querySelectorAll(".content");
-    const title = document.getElementById("aboutHeader");
     setTimeout(function(){
         for(var i = 0; i < divsToHide.length; i++){
             divsToHide[i].style.display = "none";
         }
         divsToHide[num].style.display = "block";
-        title.innerText = titles[num];
     }, 350);
 }
 
